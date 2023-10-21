b4bfc837650e904c3f8327b8a55032c26f12f13d
Yashvant Yadav <yashvantyadav855@gmail.com>
2023-10-21 16:37:57 +0530

    Implementing POST method in Springboot

--- src/main/java/com/springrest/springrest/controller/MyController.java
+++ src/main/java/com/springrest/springrest/controller/MyController.java
@@ -4,9 +4,7 @@ import com.springrest.springrest.entities.Course;
 import com.springrest.springrest.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,5 +31,11 @@ public class MyController {
             @PathVariable String courseId
     ) {
         return this.courseService.getCourse(Long.parseLong(courseId));
+    }
+
+
+    @PostMapping("/courses")
+    public Course addCourse(@RequestBody Course course) {
+        return this.courseService.addCourse(course);
     }
 }
